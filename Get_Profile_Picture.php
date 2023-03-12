6e1b92f03b0cf3b5417b513cae00f2bdd00a03e1
LaomediAz <103963097+LaomediAz@users.noreply.github.com>
2023-03-12 16:47:08 +0100

    A mettre dans le dossier dashboard

--- Get_Profile_Picture.php
+++ Get_Profile_Picture.php
@@ -4,7 +4,7 @@
   $user = "root";
   $pass = "";
   $database = "solid";
-
+  
   $selected_username = $_COOKIE["user_first_name"];
   $users_last_name = $_COOKIE["users_last_name"];
 
@@ -17,10 +17,10 @@
   while($row = mysqli_fetch_assoc($execute_command_query)){
 
     if($row["Profile_Picture"]==""){
-        echo "<img class='my_profile_pic' src='../Profile_Pictures/default.png' title='Click to change profile picture'/>";
+        echo "<img class='my_profile_pic' src='Profile_Pictures/default.png' title='Click to change profile picture'/>";
     }else{
         $picture_holder = $row["Profile_Picture"];
-        echo "<img class='my_profile_pic' src='../Profile_Pictures/$picture_holder' title='Click to change profile picture'/>";
+        echo "<img class='my_profile_pic' src='Profile_Pictures/$picture_holder' title='Click to change profile picture'/>";
     }
   }
 
